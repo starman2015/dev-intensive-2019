9c5533f78b1c8f7393691a2b36b5e607d270fa5d
Виталий Гордиенко <vgordienko@vocalize.ru>
2021-01-07 18:49:18 +0300

    *transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -16,26 +16,75 @@ object Utils {
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
-        TODO("not implemented")
+        val parts = payload.split(" ")
+        val partsStringBuilder = StringBuilder()
+
+        for (part in parts) {
+            val letterStringBuilder = StringBuilder()
+            val letterParts = part.toCharArray()
+            for (letter in letterParts) {
+                letterStringBuilder.append(fromRuToEng(letter.toString()))
+            }
+            letterStringBuilder.append(divider)
+            partsStringBuilder.append(letterStringBuilder.toString())
+        }
+        return partsStringBuilder.substring(0, partsStringBuilder.length - 1)
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        var firstNameInitial: String? = firstName?.trim()
-        var lastNameInitial: String? = lastName?.trim()
+        val firstNameInitial: String? = firstName?.trim()?.getOrNull(0)?.toString()?.toUpperCase()
+        val lastNameInitial: String? = lastName?.trim()?.getOrNull(0)?.toString()?.toUpperCase()
 
-        if (firstNameInitial != null) {
-            firstNameInitial =
-                if (firstNameInitial.isNotBlank()) firstNameInitial.substring(0, 1).toUpperCase() else null
-        }
-
-        if (lastNameInitial != null) {
-            lastNameInitial = if (lastNameInitial.isNotBlank()) lastNameInitial.substring(0, 1).toUpperCase() else null
-        }
-
-        if (firstNameInitial == null && lastNameInitial == null) {
-            return null
+        return if (firstNameInitial == null && lastNameInitial == null) {
+            null
         } else {
-            return "${firstNameInitial ?: ""}${lastNameInitial ?: ""}"
+            "${firstNameInitial ?: ""}${lastNameInitial ?: ""}"
+        }
+    }
+
+    private fun fromRuToEng(ruLetter: String): String {
+        val pairs = mapOf(
+            "а" to "a",
+            "б" to "b",
+            "в" to "v",
+            "г" to "g",
+            "д" to "d",
+            "е" to "e",
+            "ё" to "e",
+            "ж" to "zh",
+            "з" to "z",
+            "и" to "i",
+            "й" to "i",
+            "к" to "k",
+            "л" to "l",
+            "м" to "m",
+            "н" to "n",
+            "о" to "o",
+            "п" to "p",
+            "р" to "r",
+            "с" to "s",
+            "т" to "t",
+            "у" to "u",
+            "ф" to "f",
+            "х" to "h",
+            "ц" to "c",
+            "ч" to "ch",
+            "ш" to "sh",
+            "щ" to "sh'",
+            "ъ" to "",
+            "ы" to "i",
+            "ь" to "",
+            "э" to "e",
+            "ю" to "yu",
+            "я" to "ya"
+        )
+
+        val letter: String = (pairs[ruLetter.toLowerCase()] ?: return ruLetter) ?: return ruLetter
+
+        return if (ruLetter[0].isUpperCase()) {
+            letter.capitalize()
+        } else {
+            letter
         }
     }
 }
